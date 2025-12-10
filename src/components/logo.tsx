408342334ef325382276b1ebbf701f559cb6ffd0
Javier Sayago <javier.sayago000@gmail.com>
2025-12-10 23:06:10 +0000

    ## Error Type Console Error
    ## Error Message
    A tree hydrated but some a

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,6 @@ import { cn } from '@/lib/utils';
 import { useId } from 'react';
 
 const KreaIcon = (props: React.SVGProps<SVGSVGElement>) => {
-  const gradientId = useId();
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -13,14 +12,8 @@ const KreaIcon = (props: React.SVGProps<SVGSVGElement>) => {
       height="40"
       {...props}
     >
-      <defs>
-        <linearGradient id={gradientId} x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" style={{ stopColor: 'hsl(var(--accent))', stopOpacity: 1 }} />
-          <stop offset="100%" style={{ stopColor: 'hsl(var(--primary))', stopOpacity: 1 }} />
-        </linearGradient>
-      </defs>
       <path
-        fill={`url(#${gradientId})`}
+        fill={'hsl(var(--primary))'}
         d="M25,0 L40,0 L15,30 L0,30 Z M60,0 L75,0 L100,30 L85,30 Z M75,60 L60,60 L85,30 L100,30 Z M40,60 L25,60 L0,30 L15,30 Z"
         transform="skewX(-20)"
       />
